26617ebc3ad4aa31aa7582e81941b32b4b90b4f2
fikriwado <mfikri.khoirurrizal@gmail.com>
2024-07-12 09:47:42 +0700

    update sesion table

--- database/migrations/2024_07_12_020325_create_sessions_table.php
+++ database/migrations/2024_07_12_020325_create_sessions_table.php
@@ -13,11 +13,12 @@ return new class extends Migration
     {
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
+            $table->uuid('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
             $table->integer('last_activity')->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
@@ -26,6 +27,9 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::table('sessions', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
         Schema::dropIfExists('sessions');
     }
 };
